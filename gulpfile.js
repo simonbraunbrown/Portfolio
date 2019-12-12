a929407150922999f91258114298363ca54dc7ab
simonbraunbrown <sijo.braun@googlemail.com>
2019-12-12 16:41:24 +0100

    delete webpack and setup gulp as taskrunner

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var imagemin = require("gulp-imagemin");
+var cache = require('gulp-cache');
+var browserSync = require("browser-sync").create();
+var useref = require("gulp-useref");
+var uglify = require("gulp-uglify");
+var gulpIf = require("gulp-if");
+var del = require("del");
+var runSequence = require('run-sequence');
+const { series, parallel } = require("gulp");
+
+gulp.task("hello", function() {
+  console.log("Hello Simon");
+});
+
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+    },
+    ui: {
+      port: 8010
+    },
+    port: 8080
+  });
+});
+
+gulp.task("useref", function() {
+  return gulp
+    .src("src/*.html")
+    .pipe(useref())
+    .pipe(gulpIf("*.js", uglify()))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("transfer" , function() {
+    return gulp
+    .src("src/*.html")
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("sass", function() {
+  return gulp
+    .src("src/styles/scss/*.scss")
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest("dist/"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
+
+gulp.task("fonts", function() {
+  return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task("images", function() {
+  return gulp
+    .src("src/images/**/*.+(png|jpg|gif|svg)")
+    .pipe(
+        imagemin({
+          interlaced: true
+        })
+    )
+    .pipe(gulp.dest("dist/images"));
+});
+
+gulp.task("clean:dist", function() {
+  return del.sync("dist/");
+});
+
+gulp.task("watch", gulp.series(["browserSync", "sass"]), function() {
+  gulp.watch("src/styles/scss/*.scss", gulp.series(["sass"]));
+  gulp.watch("src/*.html", browserSync.reload);
+  gulp.watch("js/js/**/*.js", browserSync.reload);
+});
+
+
+  const serve = gulp.parallel(
+    'transfer','sass', 'watch'
+  );
+
+   const build = gulp.series(
+    'clean:dist',
+    gulp.parallel('sass', 'useref', 'images', 'fonts')
+  );
+
+  exports.serve = serve;
+  exports.build = build;
