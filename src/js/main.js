283d2a9d7d6baa4c12d478f00eac5401476b9669
simonbraunbrown <sijo.braun@googlemail.com>
2020-01-12 19:42:13 +0100

    add wrapper and scroll event

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,32 @@
 console.log("hello");
 
-var imageContainer = document.querySelector(".imageContainer");
-var imageDisplay = imageContainer.querySelector(".imageDisplay");
-var images = imageContainer.querySelectorAll(".image");
+var imageContainer = document.querySelector('.imageContainer');
+var imageDisplay = imageContainer.querySelector('.imageDisplay');
+var images = imageContainer.querySelectorAll('.image');
+var button = document.querySelector('.closeButton');
 
 images.forEach(image => {
-  image.addEventListener("click", () => {
+  image.addEventListener('click', () => {
     const backgroundImageURL = image.style.backgroundImage;
     const splitURL = backgroundImageURL.split('"');
     if (imageDisplay.querySelector(".imageToShow")) {
-      var img = imageDisplay.querySelector(".imageToShow"); 
+      var img = imageDisplay.querySelector('.imageToShow'); 
       var newImg = document.createElement("img");
-      newImg.className = "imageToShow";
+      newImg.className = 'imageToShow';
       newImg.src = splitURL[1];
       imageDisplay.replaceChild(newImg, img);
+      imageDisplay.style.display = 'block';
     } else {
       var img = document.createElement("img");
-      img.className = "imageToShow";
+      img.className = 'imageToShow';
       img.src = splitURL[1];
       imageDisplay.appendChild(img);
+      imageDisplay.style.display = 'block';
     }
+    imageDisplay.scrollIntoView({behavior: "smooth", block:"end"});
   });
+});
+
+button.addEventListener('click', () => {
+    imageDisplay.style.display = 'none';
 });
