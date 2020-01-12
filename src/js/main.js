9900033b5c2bc58687522443e5d8211664ac25d6
simonbraunbrown <sijo.braun@googlemail.com>
2020-01-12 19:46:11 +0100

    cleanup and reformat

--- src/js/main.js
+++ src/js/main.js
@@ -6,25 +6,25 @@ var images = imageContainer.querySelectorAll('.image');
 var button = document.querySelector('.closeButton');
 
 images.forEach(image => {
-  image.addEventListener('click', () => {
-    const backgroundImageURL = image.style.backgroundImage;
-    const splitURL = backgroundImageURL.split('"');
-    if (imageDisplay.querySelector(".imageToShow")) {
-      var img = imageDisplay.querySelector('.imageToShow'); 
-      var newImg = document.createElement("img");
-      newImg.className = 'imageToShow';
-      newImg.src = splitURL[1];
-      imageDisplay.replaceChild(newImg, img);
-      imageDisplay.style.display = 'block';
-    } else {
-      var img = document.createElement("img");
-      img.className = 'imageToShow';
-      img.src = splitURL[1];
-      imageDisplay.appendChild(img);
-      imageDisplay.style.display = 'block';
-    }
-    imageDisplay.scrollIntoView({behavior: "smooth", block:"end"});
-  });
+    image.addEventListener('click', () => {
+        const backgroundImageURL = image.style.backgroundImage;
+        const splitURL = backgroundImageURL.split('"');
+        if (imageDisplay.querySelector('.imageToShow')) {
+            var img = imageDisplay.querySelector('.imageToShow');
+            var newImg = document.createElement("img");
+            newImg.className = 'imageToShow';
+            newImg.src = splitURL[1];
+            imageDisplay.replaceChild(newImg, img);
+            imageDisplay.style.display = 'block';
+        } else {
+            var img = document.createElement("img");
+            img.className = 'imageToShow';
+            img.src = splitURL[1];
+            imageDisplay.appendChild(img);
+            imageDisplay.style.display = 'block';
+        }
+        imageDisplay.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    });
 });
 
 button.addEventListener('click', () => {
