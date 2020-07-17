056293e5a044f28ec83f6baf18d16088da18a145
simonbraunbrown <sijo.braun@googlemail.com>
2020-07-17 13:53:26 +0200

    some styling changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-console.log("hello");
 
 var imageContainer = document.querySelector('.imageContainer');
 var imageDisplay = imageContainer.querySelector('.imageDisplay');
 var images = imageContainer.querySelectorAll('.image');
 var button = document.querySelector('.closeButton');
+const canvasHeight = 150;
 
 images.forEach((image) => {
     image.addEventListener('click', () => {
@@ -35,16 +35,17 @@ button.addEventListener('click', () => {
 
 const s = (sketch) => {
     sketch.setup = () => {
-        var canvas = sketch.createCanvas(sketch.windowWidth, 100);
+        var canvas = sketch.createCanvas(sketch.windowWidth, canvasHeight);
         // canvas.parent("sketch-footer");
         canvas.style("display", "block");
         sketch.background(0);
         sketch.strokeWeight(10);
         sketch.stroke(128);
+        sketch.frameRate(10);
     }
 
     sketch.windowResized = () => {
-        sketch.resizeCanvas(sketch.windowWidth, 100);
+        sketch.resizeCanvas(sketch.windowWidth, canvasHeight);
     }
 
     sketch.draw = () => {
@@ -56,16 +57,20 @@ let myp5Footer = new p5(s, 'sketch-footer');
 
 myp5Header.draw = () => {
     myp5Header.background(0, 50);
+    myp5Header.frameRate(10);
 
         if (myp5Header.frameCount % 60 == 0) {
+            myp5Header.frameRate(60);
             myp5Header.line(myp5Header.random(0, myp5Header.width), 0, myp5Header.mouseX, myp5Header.mouseY);
         }
 } 
 
 myp5Footer.draw = () => {
     myp5Footer.background(0, 50);
+    myp5Footer.frameRate(10);
 
         if (myp5Footer.frameCount % 60 == 5) {
-                myp5Footer.line(myp5Footer.random(0, myp5Footer.width), 100, myp5Footer.mouseX, myp5Footer.mouseY);
+            myp5Footer.frameRate(60);
+            myp5Footer.line(myp5Footer.random(0, myp5Footer.width), canvasHeight, myp5Footer.mouseX, myp5Footer.mouseY);
         }
 } 
