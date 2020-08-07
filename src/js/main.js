ca78a581bcde61af569f5316e7425d6008e1bbb2
simonbraunbrown <sijo.braun@googlemail.com>
2020-08-07 21:28:21 +0200

    restructure add video change display mode change layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,84 @@
 
-var imageContainer = document.querySelector('.imageContainer');
-var imageDisplay = imageContainer.querySelector('.imageDisplay');
-var images = imageContainer.querySelectorAll('.image');
-var button = document.querySelector('.closeButton');
+let imageContainer = document.querySelector('.imageContainer');
+let imageDisplay = imageContainer.querySelector('.imageDisplay');
+let images = imageContainer.querySelectorAll('.image');
+let imageWrappers = imageContainer.querySelectorAll('.imageWrapper');
+let button = document.querySelector('.closeButton');
 const canvasHeight = 150;
 
-images.forEach((image) => {
-    image.addEventListener('click', () => {
-        const backgroundImageURL = image.style.backgroundImage;
-        const splitURL = backgroundImageURL.split('"');
-        if (imageDisplay.querySelector('.imageToShow')) {
-            var img = imageDisplay.querySelector('.imageToShow');
-            var newImg = document.createElement("img");
-            newImg.className = 'imageToShow';
-            newImg.src = splitURL[1];
-            imageDisplay.replaceChild(newImg, img);
-            imageDisplay.style.display = 'block';
-        } else {
-            var img = document.createElement("img");
-            img.className = 'imageToShow';
-            img.src = splitURL[1];
-            imageDisplay.appendChild(img);
-            imageDisplay.style.display = 'block';
+imageWrappers.forEach((wrapper) =>{
+    const self = wrapper;
+    wrapper.querySelector('.image').addEventListener('click', (e) =>{
+        if (self.className !=('imageWrapper imageWrapper--expand')) {
+            expand(self);
         }
-        imageDisplay.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        else {
+            squeeze(self);
+        }
     });
 });
+
+// images.forEach((image) => {
+//     image.addEventListener('click', () => {
+//         const backgroundImageURL = image.style.backgroundImage;
+//         const splitURL = backgroundImageURL.split('"');
+//         if (imageDisplay.querySelector('.imageToShow')) {
+//             let img = imageDisplay.querySelector('.imageToShow');
+//             let newImg = document.createElement('img');
+//             newImg.className = 'imageToShow';
+//             newImg.src = splitURL[1];
+//             imageDisplay.replaceChild(newImg, img);
+//             imageDisplay.style.display = 'block';
+//         } else {
+//             let img = document.createElement('img');
+//             img.className = 'imageToShow';
+//             img.src = splitURL[1];
+//             imageDisplay.appendChild(img);
+//             imageDisplay.style.display = 'block';
+//         }
+//         imageDisplay.scrollIntoView({ behavior: 'smooth', block: 'end' });
+//     });
+// });
 
 button.addEventListener('click', () => {
     imageDisplay.style.display = 'none';
 });
 
+function expand(wrapper) {
+    wrapper.classList.add('imageWrapper--expand');
+    const buttonWrapper = document.createElement('div');
+    buttonWrapper.className = 'buttonWrapper';
+    const button = document.createElement('button');
+    button.className = 'closeButton';
+    button.addEventListener('click', (e) => {
+        squeeze(wrapper);
+    });
+    buttonWrapper.appendChild(button)
+    wrapper.appendChild(buttonWrapper);
+    wrapper.scrollIntoView({ behavior: 'smooth', block: 'start' });
+};
 
+function squeeze(wrapper) {
+    wrapper.classList.remove('imageWrapper--expand');
+}
 
 const s = (sketch) => {
+    let canvas
+
+    function centerCanvas() {
+        let x = (sketch.windowWidth - sketch.width) / 2;
+        let y = (sketch.windowHeight - sketch.height) / 2;
+        canvas.position(x, y);
+    }
+
     sketch.setup = () => {
-        var canvas = sketch.createCanvas(sketch.windowWidth, canvasHeight);
-        // canvas.parent("sketch-footer");
+        canvas = sketch.createCanvas(sketch.windowWidth, canvasHeight);
+        console.log(sketch.windowWidth, sketch.windowHeight);
+        //canvas.parent("sketch-footer");
         canvas.style("display", "block");
+        canvas.style("max-width", "100%");
         sketch.background(0);
-        sketch.strokeWeight(10);
+        sketch.strokeWeight(3);
         sketch.stroke(128);
         sketch.frameRate(10);
     }
@@ -52,18 +91,19 @@ const s = (sketch) => {
     }
 }
 
-let myp5Header = new p5(s, 'sketch-header');
+// let myp5Header = new p5(s, 'sketch-header');
+
+// myp5Header.draw = () => {
+//     myp5Header.background(0, 50);
+//     myp5Header.frameRate(10);
+
+//         if (myp5Header.frameCount % 60 == 0) {
+//             myp5Header.frameRate(60);
+//             myp5Header.line(myp5Header.random(0, myp5Header.width), 0, myp5Header.mouseX, myp5Header.mouseY);
+//         }
+// } 
+
 let myp5Footer = new p5(s, 'sketch-footer');
-
-myp5Header.draw = () => {
-    myp5Header.background(0, 50);
-    myp5Header.frameRate(10);
-
-        if (myp5Header.frameCount % 60 == 0) {
-            myp5Header.frameRate(60);
-            myp5Header.line(myp5Header.random(0, myp5Header.width), 0, myp5Header.mouseX, myp5Header.mouseY);
-        }
-} 
 
 myp5Footer.draw = () => {
     myp5Footer.background(0, 50);
@@ -71,6 +111,9 @@ myp5Footer.draw = () => {
 
         if (myp5Footer.frameCount % 60 == 5) {
             myp5Footer.frameRate(60);
-            myp5Footer.line(myp5Footer.random(0, myp5Footer.width), canvasHeight, myp5Footer.mouseX, myp5Footer.mouseY);
+            for(let i = 0; i < 10; i++) {
+                myp5Footer.line(myp5Footer.random(0, myp5Footer.width), canvasHeight, myp5Footer.mouseX, myp5Footer.mouseY);
+                //myp5Footer.line(myp5Footer.random(0, myp5Footer.width), canvasHeight, myp5Footer.mouseX, myp5Footer.mouseY);
+            }
         }
 } 
