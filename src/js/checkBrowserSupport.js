cd1c796d63c552ae0665cf07464568e5386e503c
simonbraunbrown <sijo.braun@googlemail.com>
2020-08-24 14:44:47 +0200

    add check for Browser Support

---
+++ src/js/checkBrowserSupport.js
@@ -0,0 +1,4 @@
+if (!NodeList.prototype.forEach) {
+  document.body.innerHTML =
+    '<div style="position: absolute; width: 100%; height: 100%; background-color: #161616"><div style="margin: 20% auto; font-size: 2em; text-transform: uppercase; text-align: center; color: #d6d6d6;">your browser is not supported!<br>please consider to use a contemporary browser...</div></div>';
+}
