290bb14bf0ee9e66d382ab5b9c9ad95ecb158db6
simonbraunbrown <sijo.braun@googlemail.com>
2020-08-27 20:57:35 +0200

    setup

--- src/js/headeranimation.js
+++ src/js/headeranimation.js
@@ -1,1 +1,44 @@
 import * as THREE from './vendor/three/three.min.js';
+
+let time = 0;
+
+let scene;
+let camera;
+let light;
+let mesh;
+let renderer;
+
+function init() {
+  let container = document.querySelector('.canvasContainer');
+
+  createScene();
+  createCamera();
+  createLight();
+  createMesh();
+  createRenderer();
+}
+
+function createScene() {}
+
+function createCamera() {}
+
+function createLight() {}
+
+function createMesh() {}
+
+function createRenderer() {}
+
+function update() {}
+
+function render() {}
+
+function play() {
+  renderer.setAnimationLoop(() => {
+    update();
+    render();
+  });
+}
+
+function stop() {
+  renderer.setAnimationLoop(null);
+}
