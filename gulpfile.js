5eb95f7299ab0ba6d756279788c47d295136480d
simonbraunbrown <sijo.braun@googlemail.com>
2020-09-03 13:56:54 +0200

    remove babel task and refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const {series, parallel, watch, task } = require('gulp');
+const { series, parallel, watch, task } = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoPrefixer = require('gulp-autoprefixer');
@@ -16,155 +16,166 @@ const del = require('del');
 const runSequence = require('run-sequence');
 
 const handleJS = lazypipe()
-  .pipe(babel, {
-    presets: ['@babel/preset-env']
-  })
-  .pipe(terser);
+	.pipe(babel, {
+		presets: ['@babel/preset-env'],
+	})
+	.pipe(terser);
 
-gulp.task('hello', function() {
-  console.log('Hello Simon');
+gulp.task('hello', function () {
+	console.log('Hello Simon');
 });
 
 const AUTOPREFIXER_BROWSERS = [
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
+	'ie >= 10',
+	'ie_mob >= 10',
+	'ff >= 30',
+	'chrome >= 34',
+	'safari >= 7',
+	'opera >= 23',
+	'ios >= 7',
+	'android >= 4.4',
+	'bb >= 10',
 ];
 
- gulp.task('sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'src'
-    },
-    ui: {
-      port: 8010
-    },
-    port: 8080
-  });
+gulp.task('sync', function () {
+	browserSync.init({
+		server: {
+			baseDir: 'src',
+		},
+		ui: {
+			port: 8010,
+		},
+		port: 8080,
+	});
 });
 
- gulp.task('ref', function() {
-  return gulp
-    .src('src/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('**/js/*.js', handleJS()))
-    .pipe(gulpIf('**/vendor/**/*.js', terser()))
-    .pipe(gulpIf('*.css', cleanCSS()))
-    .pipe(gulp.dest('dist'));
+gulp.task('ref', function () {
+	return gulp
+		.src('src/*.html')
+		.pipe(useref())
+		.pipe(gulpIf('**/js/*.js', terser()))
+		.pipe(gulpIf('**/vendor/**/*.js', terser()))
+		.pipe(gulpIf('*.css', cleanCSS()))
+		.pipe(gulp.dest('dist'));
 });
 
- gulp.task('handleSass', function() {
-  return gulp
-    .src('src/styles/scss/*.scss')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(autoPrefixer())
-    .pipe(gulp.dest('src/styles/css'))
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
-});
-
- gulp.task('html', function() {
-  return gulp
-    .src('src/*.html')
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
-});
-
- gulp.task('js', function() {
-  return gulp
-    .src('src/js/*.js')
-    .pipe(
-      browserSync.reload({
-        stream: true
-      })
-    );
-});
-
- gulp.task('fonts', function() {
-  return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('models', function() {
-  return gulp.src('src/models/*').pipe(gulp.dest('dist/models'));
-});
-
- gulp.task('images', function() {
-  return gulp
-    .src('src/images/**/*.+(png|jpg|gif|svg)')
-    .pipe(imageMin([
-			//gif minify
-			imageMin.gifsicle({interlaced: true}),
-			//jpg minify
-			imageMin.jpegtran({progressive: true}),
-			//png minify
-			imageMin.optipng({optimizationLevel: 5}),
-			//svg minify
-			imageMin.svgo({
-				plugins: [
-					{removeViewBox: true},
-					{cleanupIDs: false}
-				]
+gulp.task('handleSass', function () {
+	return gulp
+		.src('src/styles/scss/*.scss')
+		.pipe(sass()) // Using gulp-sass
+		.pipe(autoPrefixer())
+		.pipe(gulp.dest('src/styles/css'))
+		.pipe(
+			browserSync.reload({
+				stream: true,
 			})
-			],{verbose: true}))
-    .pipe(gulp.dest('dist/images'));
+		);
 });
 
-  gulp.task('videos', function() {
-  return gulp
-  .src('src/videos/*.mp4')
-  .pipe(gulp.dest('dist/videos'));
+gulp.task('html', function () {
+	return gulp.src('src/*.html').pipe(
+		browserSync.reload({
+			stream: true,
+		})
+	);
 });
 
-  gulp.task('cleanDist', async function() {
-  return del.sync('dist/*');
+gulp.task('js', function () {
+	return gulp.src('src/js/*.js').pipe(
+		browserSync.reload({
+			stream: true,
+		})
+	);
 });
 
- gulp.task('observe', function() {
-  gulp.watch(['src/styles/scss/*.scss'], gulp.series('handleSass'));
-  gulp.watch(['src/*.html'], gulp.series('html'));
-  gulp.watch(['src/js/*.js'], gulp.series('js'));
+gulp.task('fonts', function () {
+	return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('autoPrefix', function() {
-  gulp.src('src/styles/css/style.css')
-      .pipe(autoPrefixer({
-          cascade: false
-      }))
-      .pipe(gulp.dest('dist/'))
-    });
+gulp.task('models', function () {
+	return gulp.src('src/models/*').pipe(gulp.dest('dist/models'));
+});
 
-  gulp.task('makeUgly', function() {
-  gulp.src('src/js/main.js')
-      .pipe(terser())
-      .pipe(rename({ extname: '.min.js' }))
-      .pipe(gulp.dest('dist/'))
+gulp.task('images', function () {
+	return gulp
+		.src('src/images/**/*.+(png|jpg|gif|svg)')
+		.pipe(
+			imageMin(
+				[
+					//gif minify
+					imageMin.gifsicle({ interlaced: true }),
+					//jpg minify
+					imageMin.jpegtran({ progressive: true }),
+					//png minify
+					imageMin.optipng({ optimizationLevel: 5 }),
+					//svg minify
+					imageMin.svgo({
+						plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+					}),
+				],
+				{ verbose: true }
+			)
+		)
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('videos', function () {
+	return gulp.src('src/videos/*.mp4').pipe(gulp.dest('dist/videos'));
+});
+
+gulp.task('cleanDist', async function () {
+	return del.sync('dist/*');
+});
+
+gulp.task('observe', function () {
+	gulp.watch(['src/styles/scss/*.scss'], gulp.series('handleSass'));
+	gulp.watch(['src/*.html'], gulp.series('html'));
+	gulp.watch(['src/js/*.js'], gulp.series('js'));
+});
+
+gulp.task('autoPrefix', function () {
+	gulp
+		.src('src/styles/css/style.css')
+		.pipe(
+			autoPrefixer({
+				cascade: false,
+			})
+		)
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('makeUgly', function () {
+	gulp
+		.src('src/js/main.js')
+		.pipe(terser())
+		.pipe(rename({ extname: '.min.js' }))
+		.pipe(gulp.dest('dist/'));
 });
 
 gulp.task('handleJS', () =>
-    gulp.src('src/js/main.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(gulp.dest('dist/'))
+	gulp
+		.src('src/js/main.js')
+		.pipe(
+			babel({
+				presets: ['@babel/env'],
+			})
+		)
+		.pipe(gulp.dest('dist/'))
 );
 
 // [] --> paralell ()--> sequence
 
-const serve = gulp.parallel('hello','handleSass', 'sync', 'observe');
+const serve = gulp.parallel('hello', 'handleSass', 'sync', 'observe');
 
-const build = gulp.series('cleanDist', 'handleSass','ref', 'fonts', 'models', 'images', 'videos');
+const build = gulp.series(
+	'cleanDist',
+	'handleSass',
+	'ref',
+	'fonts',
+	'models',
+	'images',
+	'videos'
+);
 
 exports.serve = serve;
 
