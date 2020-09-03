6ea80eba66412c2b33a74488b13b6cbeea4ecc21
simonbraunbrown <sijo.braun@googlemail.com>
2020-09-03 13:52:53 +0200

    add load texture function and enviroment map

--- src/js/headeranimation.js
+++ src/js/headeranimation.js
@@ -10,145 +10,175 @@ let renderer;
 let controls;
 
 function init() {
-  window.addEventListener('resize', onWindowResize);
-  container = document.querySelector('.canvasContainer');
+	window.addEventListener('resize', onWindowResize);
+	container = document.querySelector('.canvasContainer');
 
-  createScene();
-  createCamera();
-  createLight();
-  createMesh();
-  loadModel();
-  createRenderer();
-  createControls();
+	createScene();
+	createCamera();
+	createLight();
+	createMesh();
+	loadModel();
+	createRenderer();
+	createControls();
 }
 
 function createScene() {
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x161616);
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0x161616);
 }
 
 function createCamera() {
-  const fov = 75;
-  const aspect = container.clientWidth / container.clientHeight;
-  const near = 0.1;
-  const far = 5;
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 0, 1.5);
+	const fov = 75;
+	const aspect = container.clientWidth / container.clientHeight;
+	const near = 0.1;
+	const far = 5;
+	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.set(0, 0, 1.5);
 }
 
 function createLight() {
-  const color = 0xffffff;
-  const intensity = 1;
-  const light = new THREE.DirectionalLight(color, intensity);
-  light.position.set(-10, 2, 4);
-  scene.add(light);
+	const color = 0x404040;
+	const intensity = 1.0;
+	const light = new THREE.AmbientLight(color, intensity);
+	//light.position.set(-10, 2, 4);
+	scene.add(light);
 }
 
 function createMesh() {
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+	const boxWidth = 1;
+	const boxHeight = 1;
+	const boxDepth = 1;
+	const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshStandardMaterial({ color: 0xd6d6d6 });
+	const material = new THREE.MeshStandardMaterial({ color: 0xd6d6d6 });
 
-  mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(0, 0, 0);
-  //scene.add(mesh);
+	mesh = new THREE.Mesh(geometry, material);
+	mesh.position.set(0, 0, 0);
+	//scene.add(mesh);
+}
+
+function loadTexture(_url) {
+	const loader = new THREE.TextureLoader();
+	const url = _url;
+	let texture;
+
+	const addTexture = (res) => {};
+
+	const onError = (e) => {
+		console.log(e);
+	};
+	texture = loader.load(
+		url,
+		(response) => addTexture(response),
+		undefined,
+		onError
+	);
+	console.log(texture);
+	return texture;
 }
 
 function loadModel() {
-  const loader = new THREE.GLTFLoader();
-  const url = '../models/me_lowPoly.glb';
-  const modelPosition = new THREE.Vector3(0, 0, 0);
+	const loader = new THREE.GLTFLoader();
+	const url = '../models/me_lowPoly.glb';
+	const modelPosition = new THREE.Vector3(0, 0, 0);
 
-  const addModel = (gltf, position) => {
-    console.log(gltf);
-    model = gltf.scene.children[0];
-    model.position.copy(position);
+	const addModel = (gltf, position) => {
+		model = gltf.scene.children[0];
+		model.position.copy(position);
 
-    const material = new THREE.MeshStandardMaterial({
-      color: 0xd6d6d6,
-      flatShading: true,
-      roughness: 0.35,
-      metalness: 1.0
-    });
+		const envMap = loadTexture('../models/Warehouse-with-lights.jpg');
+		envMap.mapping = THREE.EquirectangularReflectionMapping;
+		envMap.encoding = THREE.sRGBEncoding;
 
-    model.material.copy(material);
+		const material = new THREE.MeshStandardMaterial({
+			color: 0x161616,
+			flatShading: true,
+			roughness: 0.05,
+			metalness: 1.0,
+			envMap: envMap,
+			envMapIntensity: 1.5,
+			morphNormals: true,
+		});
 
-    scene.add(model);
-    play();
-  };
+		const matMaterial = new THREE.MeshMatcapMaterial({
+			color: 0x7cf5c1,
+			matcap: loadTexture('../images/displacement10.png'),
+		});
 
-  const onProgress = (progress) => {
-    //console.log(progress);
-  };
+		model.material.copy(material);
+		console.log(model);
+		scene.add(model);
+		play();
+	};
 
-  const onError = (e) => {
-    console.log(e);
-  };
+	const onProgress = (progress) => {
+		//console.log(progress);
+	};
 
-  loader.load(
-    url,
-    (response) => addModel(response, modelPosition),
-    onProgress,
-    onError
-  );
+	const onError = (e) => {
+		console.log(e);
+	};
+
+	loader.load(
+		url,
+		(response) => addModel(response, modelPosition),
+		onProgress,
+		onError
+	);
 }
 
 function createRenderer() {
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(container.clientWidth, container.clientHeight);
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(container.clientWidth, container.clientHeight);
 
-  renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setPixelRatio(window.devicePixelRatio);
 
-  container.appendChild(renderer.domElement);
+	container.appendChild(renderer.domElement);
 }
 
 function createControls() {
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.enablePan = false;
-  controls.enableZoom = false;
-  controls.enableKeys = false;
-  controls.minPolarAngle = Math.PI/2;
-  controls.maxPolarAngle = Math.PI/2;
-
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
+	controls.enableDamping = true;
+	controls.enablePan = false;
+	controls.enableZoom = false;
+	controls.enableKeys = false;
+	controls.minPolarAngle = Math.PI / 2;
+	controls.maxPolarAngle = Math.PI / 2;
 }
 
-function update() { 
-  time += 0.001;
-  const speed = 1;
-  const rot = time * speed;
+function update() {
+	time += 0.001;
+	const speed = 1;
+	const rot = time * speed;
 
-  mesh.rotation.x = rot;
-  mesh.rotation.y = rot;
-  model.rotation.y = rot;
+	mesh.rotation.x = rot;
+	mesh.rotation.y = rot;
+	model.rotation.y = rot;
 
-  controls.update(); 
+	controls.update();
 }
 
 function render() {
-  renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 function play() {
-  renderer.setAnimationLoop(() => {
-    update();
-    render();
-  });
+	renderer.setAnimationLoop(() => {
+		update();
+		render();
+	});
 }
 
 function stop() {
-  renderer.setAnimationLoop(null);
+	renderer.setAnimationLoop(null);
 }
 
 function onWindowResize() {
-  camera.aspect = container.clientWidth / container.clientHeight;
+	camera.aspect = container.clientWidth / container.clientHeight;
 
-  camera.updateProjectionMatrix();
+	camera.updateProjectionMatrix();
 
-  renderer.setSize(container.clientWidth, container.clientHeight);
+	renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
 init();
