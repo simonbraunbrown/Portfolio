dde6e5ebebed0d68720c7ac590c49183efa8c52c
simonbraunbrown <sijo.braun@googlemail.com>
2020-10-27 03:32:39 +0100

    add projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -7,9 +7,21 @@ let projects = [
 	},
 	{
 		type: 'image',
-		src: './images/nebula1.png',
-		name: 'nebula',
-		description: 'nebula',
+		src: './images/hdis.png',
+		name: 'hdis',
+		description: 'superdisplacement',
+	},
+	{
+		type: 'image',
+		src: './images/UI.png',
+		name: 'UI',
+		description: 'concept holo table',
+	},
+	{
+		type: 'image',
+		src: './images/YouTubeHistory_all.svg',
+		name: 'MYTH',
+		description: 'My YouTube History. displays videos which where watched from 2017 - 2020',
 	},
 	{
 		type: 'image',
@@ -19,14 +31,26 @@ let projects = [
 	},
 	{
 		type: 'image',
-		src: './images/nebula1.png',
-		name: 'nebula',
-		description: 'nebula',
+		src: './images/triste.png',
+		name: 'window',
+		description: 'congrete window in verdun',
+	},
+	{
+		type: 'image',
+		src: './images/root.png',
+		name: 'root',
+		description: '',
 	},
 	{
 		type: 'video',
 		src: './videos/glasParticlesColored_high422_21.mp4',
 		name: 'glasParticles',
-		description: 'glasParticles',
+		description: 'glasParticles video loop',
+	},
+	{
+		type: 'image',
+		src: './images/crate.png',
+		name: 'crate',
+		description: '',
 	},
 ];
